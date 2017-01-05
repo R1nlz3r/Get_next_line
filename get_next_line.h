e188e0c190c3f89eeb6d858f67000dbefe239206
Mathis Pandelé <mapandel@student.42.fr>
2017-01-05 14:31:26 +0100

    get_next_line.h 1.1 : Fix padding

--- get_next_line.h
+++ get_next_line.h
@@ -6,9 +6,10 @@
 
 typedef struct s_line
 {
-  int             fd;
-  char            *buf;
   struct s_line   *next;
+  int             fd;
+  char            pad_0[4];
+  char            *buf;
 }              t_line;
 
 int   get_next_line(const int fd, char **line);
